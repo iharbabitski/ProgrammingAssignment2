3b9f8c80fae91c2d82933a69871aba781a08380d
Igor <babitskyigor@gmail.com>
2014-12-21 12:51:35 +0200

    Revert "Inverse Matrix"
    This reverts commit 49e1af2b7df3de0e11020cf5932218f9b28271b4.

--- cachematrix.R
+++ cachematrix.R
@@ -1,32 +1,15 @@
-## Creates a a special function to handle matrix calculations in cache
-## 
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setsolve <- function(solve) m <<- solve
-  getsolve <- function() m
-  list(set = set, get = get,
-       setsolve = setsolve,
-       getsolve = getsolve)
-  
-  
+
 }
 
-## Returnes matrix with inverse property from cache
+
+## Write a short comment describing this function
+
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
-  m <- x$getsolve()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setsolve(m)
-  m
-  
+        ## Return a matrix that is the inverse of 'x'
 }
